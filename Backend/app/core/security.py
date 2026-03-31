44d1fcf2c881a120def64ba398e57638757fb412
iam-arjunn <arjun.n.social.media@gmail.com>
2026-04-01 01:48:03 +0530

    Authorization is working fine

--- Backend/app/core/security.py
+++ Backend/app/core/security.py
@@ -1,17 +1,25 @@
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
+import hashlib
 
-SECRET_KEY = "your-secret"
+SECRET_KEY = "Ares2009@007"
 ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
+
+def preprocess(password: str):
+    return hashlib.sha256(password.encode()).hexdigest()
+
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    processed = preprocess(password)[:72]
+    return pwd_context.hash(processed)
 
 def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+    processed = preprocess(plain)[:72]
+    return pwd_context.verify(processed, hashed)
 
 def create_access_token(data: dict):
     to_encode = data.copy()
